994787fef2d4bdfdae79521e4cb281439384a5b2
litonp <litondas7121@gmail.com>
2023-09-21 19:46:31 +0600

    mobile

---
+++ object.js
@@ -0,0 +1,23 @@
+var student = {
+    id: 115,
+    name: 'Liton Das',
+    class: 14,
+    marks: 75
+}
+
+var mobile = {
+    brand: 'Realme',
+    price: 1800,
+    storage: '128gb',
+    cemera: '48MP'
+}
+
+var myComputer = {
+    brand: 'hp',
+    price: 28000,
+    color: 'silvar',
+    procesor: 'i6'
+}
+console.log(myComputer.price);
+myComputer.procesor = 'i7';
+console.log(myComputer);
