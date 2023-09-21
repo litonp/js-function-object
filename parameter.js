a3975cc6566ce8ce88d828863472cc66d3c0bbac
litonp <litondas7121@gmail.com>
2023-09-21 17:56:57 +0600

    fera

---
+++ parameter.js
@@ -0,0 +1,19 @@
+function bringSingara(money) {
+    console.log('mama singara den');
+    console.log(money);
+
+}
+var taka = 200;
+// bringSingara(taka);
+
+function add(num1, num2) {
+    console.log('going to add', num1, num2);
+}
+// add(40, 60);
+
+function sum(a, b, c, d, e) {
+    console.log(a, b, c, d, e);
+    var sum = a + b + c + d + e;
+    console.log(sum);
+}
+sum(10, 20, 30);
