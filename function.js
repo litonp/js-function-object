14a254730c034d456c3f72ab2dfd856feb97b8e6
litonp <litondas7121@gmail.com>
2023-09-21 13:03:42 +0600

    function

---
+++ function.js
@@ -0,0 +1,11 @@
+// function declaration
+function startFan() {
+    console.log('Stand up');
+    console.log('walk towards the switch');
+    console.log('Press the switch');
+}
+
+// call the function
+startFan();
+startFan();
+startFan();
